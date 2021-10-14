1200af71558500a0b4eec6480d408d289c04379c
Marcello <marcellorocha632@gmail.com>
2021-10-14 11:11:15 -0300

    Adiciona o JavaScript

---
+++ main.js
@@ -0,0 +1,30 @@
+let today = new Date();
+let day = today.getDate();
+let month = 1 + today.getMonth();
+let year = today.getUTCFullYear();
+let inputDay = document.querySelector('#day');
+let inputMonth = document.querySelector('#month');
+let inputYear = document.querySelector('#year');
+let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const btnCalculate = document.querySelector('.btn');
+const result = document.querySelector('#result');
+
+function calculateAge() {
+    if (inputDay.value > day){  
+        day = day + months[month - 1];  
+        month = month - 1;  
+       }
+
+       if (inputMonth.value > month){  
+        month = month + 12;  
+        year = year - 1;  
+       }
+
+       var difDay = day - inputDay.value;  
+       var difMonth = month - inputMonth.value;  
+       var difYear = year - inputYear.value;
+
+       result.innerText = `You age is ${difYear} years, ${difMonth} months and ${difDay} days.`;
+}
+
+btnCalculate.addEventListener('click', calculateAge);
